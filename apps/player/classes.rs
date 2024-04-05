f0dd7422866a2d44dc5b4e95bfa2c1c34c05dcda
YJ Park <yjpark@gmail.com>
2024-04-05 15:16:59 +0800

    🚧 [WIP] - 2024-04-05 15:16:59 -

--- apps/player/classes.rs
+++ apps/player/classes.rs
@@ -12,19 +12,29 @@ class!(text_center)
  */
 Class :: from(vec! [String :: from(text_center)]),
 
-/* #265 bytes(2612..2634)
-class!(card card_body)
+/* #265 bytes(2956..2968)
+class!(card)
  */
-Class :: from(vec! [String :: from(card), String :: from(card_body)]),
+Class :: from(vec! [String :: from(card)]),
 
-/* #280 bytes(2887..2917)
+/* #270 bytes(3013..3031)
+class!(card_title)
+ */
+Class :: from(vec! [String :: from(card_title)]),
+
+/* #275 bytes(3241..3258)
+class!(card_body)
+ */
+Class :: from(vec! [String :: from(card_body)]),
+
+/* #290 bytes(3529..3559)
 class!(w_screen h_screen flex)
  */
 Class ::
 from(vec!
 [String :: from(w_screen), String :: from(h_screen), String :: from(flex)]),
 
-/* #285 bytes(2960..2972)
+/* #295 bytes(3602..3614)
 class!(w_96)
  */
 Class :: from(vec! [String :: from(w_96)]),
--- apps/player/src/views/wavesurfer.rs
+++ apps/player/src/views/wavesurfer.rs
@@ -6,12 +6,21 @@ pub fn WaveSurfer() -> Element {
     let code = r#"
         let msg = await dioxus.recv();
         console.log(msg);
-        var waveform = WaveSurfer.create({
+        var wavesurfer = WaveSurfer.create({
             container: document.getElementById("video-waveform"),
             waveColor: "rgb(200, 0, 200)",
             progressColor: "rgb(100, 0, 100)",
             media: document.getElementById("video-player"),
         });
+        // Update the zoom level on slider change
+        wavesurfer.once('decode', () => {
+            const slider = document.getElementById("video-zoom")
+
+            slider.addEventListener('input', (e) => {
+                const minPxPerSec = e.target.valueAsNumber
+                wavesurfer.zoom(minPxPerSec)
+            })
+        })
     "#;
     let mut eval = eval(code);
     let future = use_resource(move || {
@@ -22,8 +31,19 @@ pub fn WaveSurfer() -> Element {
     });
     rsx! {
         div {
-            class: class!(card card_body),
-            id: "video-waveform",
+            class: class!(card),
+            input {
+                class: class!(card_title),
+                id: "video-zoom",
+                r#type: "range",
+                min: "10",
+                max: "1000",
+                value: "100",
+            }
+            div {
+                class: class!(card_body),
+                id: "video-waveform",
+            }
         }
     }
 }

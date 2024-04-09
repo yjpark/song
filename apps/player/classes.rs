025be72ca4dd58535cad4f74fffc57b2c0fe3f56
YJ Park <yjpark@gmail.com>
2024-04-09 20:18:04 +0800

    🚧 [WIP] - 2024-04-09 20:18:04 -

--- apps/player/classes.rs
+++
@@ -1,7 +0,0 @@
-vec![
-
-/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(470), 7), ctx: SyntaxContextId(229446) }
-flex flex_col w_64 my_2
- */
-Class :: from (vec ! [String :: from (flex) , String :: from (flex_col) , String :: from (w_64) , String :: from (my_2)]),
-
--- apps/player/css/classes.rs
+++ apps/player/css/classes.rs
@@ -1,58 +1,58 @@
 vec![
 
-/* #235 bytes(1109..1142)
+/* #234 bytes(1109..1142)
 class!(card card_body "video-js")
  */
 Class ::
 from(vec!
 [String :: from(card), String :: from(card_body), String :: from("video-js")]),
 
-/* #255 bytes(2006..2025)
+/* #254 bytes(2006..2025)
 class!(text_center)
  */
 Class :: from(vec! [String :: from(text_center)]),
 
-/* #271 bytes(2831..2845)
+/* #270 bytes(2831..2845)
 class!(w_full)
  */
 Class :: from(vec! [String :: from(w_full)]),
 
-/* #276 bytes(2888..2912)
+/* #275 bytes(2888..2912)
 class!(w_auto h_16 mx_2)
  */
 Class ::
 from(vec!
 [String :: from(w_auto), String :: from(h_16), String :: from(mx_2)]),
 
-/* #303 bytes(3664..3686)
+/* #302 bytes(3664..3686)
 class!(timeline_start)
  */
 Class :: from(vec! [String :: from(timeline_start)]),
 
-/* #308 bytes(3823..3846)
+/* #307 bytes(3823..3846)
 class!(timeline_middle)
  */
 Class :: from(vec! [String :: from(timeline_middle)]),
 
-/* #313 bytes(4060..4093)
+/* #312 bytes(4060..4093)
 class!(timeline_end timeline_box)
  */
 Class ::
 from(vec! [String :: from(timeline_end), String :: from(timeline_box)]),
 
-/* #318 bytes(3372..3403)
+/* #317 bytes(3372..3403)
 class!(timeline justify_center)
  */
 Class :: from(vec! [String :: from(timeline), String :: from(justify_center)]),
 
-/* #337 bytes(4620..4646)
+/* #336 bytes(4620..4646)
 class!(flex flex_wrap m_8)
  */
 Class ::
 from(vec!
 [String :: from(flex), String :: from(flex_wrap), String :: from(m_8)]),
 
-/* #365 bytes(5040..5071)
+/* #364 bytes(5040..5071)
 class!(flex flex_col w_64 my_2)
  */
 Class ::
@@ -60,29 +60,29 @@ from(vec!
 [String :: from(flex), String :: from(flex_col), String :: from(w_64), String
 :: from(my_2)]),
 
-/* #396 bytes(5633..5654)
+/* #395 bytes(5633..5654)
 class!(flex flex_row)
  */
 Class :: from(vec! [String :: from(flex), String :: from(flex_row)]),
 
-/* #490 bytes(7550..7580)
+/* #489 bytes(7550..7580)
 class!(w_screen h_screen flex)
  */
 Class ::
 from(vec!
 [String :: from(w_screen), String :: from(h_screen), String :: from(flex)]),
 
-/* #495 bytes(7623..7637)
+/* #494 bytes(7623..7637)
 class!(w_full)
  */
 Class :: from(vec! [String :: from(w_full)]),
 
-/* #510 bytes(8194..8219)
+/* #509 bytes(8194..8219)
 class!(w_screen h_screen)
  */
 Class :: from(vec! [String :: from(w_screen), String :: from(h_screen)]),
 
-/* #522 bytes(8437..8467)
+/* #521 bytes(8437..8467)
 class!(w_screen h_screen flex)
  */
 Class ::

c3345288f7d0046711fdc8a64ca235370244ab38
YJ Park <yjpark@gmail.com>
2024-04-09 20:12:38 +0800

    🚧 [WIP] - 2024-04-09 20:12:38 -

---
+++ apps/player/build.rs
@@ -0,0 +1,10 @@
+use std::env;
+use std::path::Path;
+use dioxus_daisyui::build::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let current_dir = env::current_dir()?;
+    let css_dir = Path::new(&current_dir).join("css");
+    let classes_path = Path::new(&css_dir).join("classes.rs");
+    check_classes(classes_path)
+}
